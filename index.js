c9de39fd742b6402d7062501961ef424ec47cf2d
Nicolas <nic.truong@gmail.com>
2016-06-17 19:33:51 +0000

    added the audio stream

--- index.js
+++ index.js
@@ -8,34 +8,34 @@ var child;
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('HELLO WORLD!');
+	res.send('HELLO WORLD!');	
 });
 
 app.put('/tv', function(req, res) {
-	child = exec('echo "on 0" | cec-client -s', function(error, stdout, stderr) {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (error !== null) {
-			console.log('exec error: ' + error);
-			res.sendStatus(500);
-		}else{		
+//	child = exec('echo "on 0" | cec-client -s', function(error, stdout, stderr) {
+//		console.log('stdout: ' + stdout);
+//		console.log('stderr: ' + stderr);
+//		if (error !== null) {
+//			console.log('exec error: ' + error);
+//			res.sendStatus(500);
+//		}else{		
 			res.sendStatus(200);
-		}
-	});
+//		}
+//	});
 });
 
 
 app.delete('/tv', function(req, res) {
-	child = exec('echo "standby 0" | cec-client -s', function(error, stdout, stderr) {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (error !== null) {
-			console.log('exec error: ' + error);
-			res.sendStatus(500);
-		}else{		
+//	child = exec('echo "standby 0" | cec-client -s', function(error, stdout, stderr) {
+//		console.log('stdout: ' + stdout);
+//		console.log('stderr: ' + stderr);
+//		if (error !== null) {
+//			console.log('exec error: ' + error);
+//			res.sendStatus(500);
+//		}else{		
 			res.sendStatus(200);
-		}
-	});
+//		}
+//	});
 });
 
 app.put('/tv/channels/:channel', function(req, res) {
@@ -52,4 +52,27 @@ app.put('/tv/channels/:channel/record', function(req, res) {
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
+
+/////////////////
+/////////////////
+
+var http = require("http");
+var fileSystem = require("fs");
+var path = require("path");
+var util = require("util");
+
+http.createServer(function(request, response) {
+	var filePath = "1.mp3";
+	var stat = fileSystem.statSync(filePath);
+
+	response.writeHead(200, {
+		'Content-Type': 'audio/mpeg',
+		'Content-Length': stat.size	
+	});
+
+	var readStream = fileSystem.createReadStream(filePath);
+	util.pump(readStream, response);
+	
+}).listen(2000);
+
 
